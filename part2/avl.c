875cfff816d29b2f3001eea277680a703a1e5e03
bestronaldo <apadalko90@gmail.com>
2026-05-09 16:52:25 +0300

    [ Part 2 ] AVL-дерево
    Реализация AVL-дерева

---
+++ part2/avl.c
@@ -0,0 +1,168 @@
+#include "avl.h"
+
+static int max(int a, int b) {
+	if (a >= b) return a;
+
+	return b;
+}
+
+static void Update(AVLnode* node) {
+	int hl = GetHeight(GetLeft(node));
+	int hr = GetHeight(GetRight(node));
+	if (hl < hr)
+		hl = hr;
+
+	node->height = hl + 1;
+}
+
+static AVLnode* RightRotate(AVLnode* node) {
+	if (!node || !GetLeft(node))	return node;
+
+	AVLnode* left	= GetLeft(node);
+	node->left		= GetRight(left);
+	left->right		= node;
+
+	Update(node);
+	Update(left);
+
+	return left;
+}
+
+static AVLnode* LeftRotate(AVLnode* node) {
+	if (!node || !GetRight(node))	return node;
+
+	AVLnode* right	= GetRight(node);
+	node->right		= GetLeft(right);
+	right->left		= node;
+
+	Update(node);
+	Update(right);
+
+	return right;
+}
+
+static AVLnode* BigRightRotate(AVLnode* node) {
+	node->left = LeftRotate(GetLeft(node));
+	Update(node);
+
+	return RightRotate(node);
+}
+
+static AVLnode* BigLeftRotate(AVLnode* node) {
+	node->right = RightRotate(GetRight(node));
+	Update(node);
+
+	return LeftRotate(node);
+}
+
+AVLnode* Insert(AVLnode* node, int val) {
+	if (node == NULL)   return AVLnodeCtor(val);
+
+	if (val < node->val) {
+		node->left = Insert(node->left, val);
+	} else if (val > node->val) {
+		node->right = Insert(node->right, val);
+	} else {
+		return node;
+	}
+
+	Update(node);
+	int bal = GetBal(node);
+
+	if (bal > 1 && val > GetRight(node)->val)
+		return LeftRotate(node);
+	
+	if (bal < -1 && val < GetLeft(node)->val)
+		return RightRotate(node);
+
+	if (bal > 1 && val < GetRight(node)->val)
+		return BigLeftRotate(node);
+	
+	if (bal < -1 && val > GetLeft(node)->val)
+		return BigRightRotate(node);
+	
+	return node;
+}
+
+static AVLnode* MinValNode(AVLnode* node) {
+	AVLnode* current = node;
+	while (current->left != NULL)
+		current = current->left;
+	return current;
+}
+
+AVLnode* Delete(AVLnode* node, int val) {
+	if (node == NULL) return node;
+
+	if (val < node->val) {
+		node->left	= Delete(GetLeft(node), val);
+	} else if (val > node->val) {
+		node->right	= Delete(GetRight(node), val);
+	} else {
+		AVLnode* last_node = node;
+
+		if (GetLeft(node) == NULL) {
+			node = GetRight(node);
+			free(last_node);
+		} else if (GetRight(node) == NULL) {
+			node = GetLeft(node);
+			free(last_node);
+		} else {
+			node->val	= MinValNode(GetRight(node))->val;
+			node->right = Delete(GetRight(node), node->val);
+		}
+	}
+
+	if (!node)	return node;
+
+	Update(node);
+	int bal = GetBal(node);
+
+	if (bal < -1 && GetBal(GetLeft(node)) <= 0)
+		return RightRotate(node);
+
+	if (bal < -1 && GetBal(GetLeft(node)) > 0)
+		return BigRightRotate(node);
+
+	if (bal > 1 && GetBal(GetRight(node)) >= 0)
+		return LeftRotate(node);
+
+	if (bal > 1 && GetBal(GetRight(node)) > 0)
+		return BigLeftRotate(node);
+
+	return node;
+}
+
+AVLnode* AVLnodeCtor(int val) {
+	AVLnode* node = (AVLnode*)calloc(1, sizeof(AVLnode));
+
+	node->val    = val;
+	node->left   = node->right = NULL;
+	node->height = 1;
+
+	return node;
+}
+
+int GetHeight(AVLnode *node) {
+	if (!node)  return 0;
+
+	return node->height;
+}
+
+AVLnode* GetLeft(AVLnode* node) {
+	if (!node)  return NULL;
+
+	return node->left;
+}
+
+AVLnode* GetRight(AVLnode* node) {
+	if (!node)  return NULL;
+
+	return node->right;
+}
+
+int GetBal(AVLnode* node) {
+	if (!node)  return 0;
+
+	return GetHeight(GetRight(node)) - GetHeight(GetLeft(node));
+}
---
+++ part2/avl.h
@@ -0,0 +1,25 @@
+#ifndef _AVL_H_
+#define _AVL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct AVLnode {
+	int		 val;
+	int		 height;
+	AVLnode* left;
+	AVLnode* right;
+} AVLnode;
+
+AVLnode*	Insert		(AVLnode* node, int val);
+AVLnode*	Delete		(AVLnode* root, int val);
+
+AVLnode*	AVLnodeCtor	(int val)				;
+void		NodeDtor	(AVLnode* node)			;
+
+int			GetHeight	(AVLnode *node)			;
+AVLnode*	GetLeft		(AVLnode* node)			;
+AVLnode*	GetRight	(AVLnode* node)			;
+int			GetBal		(AVLnode* node)			;
+
+#endif
