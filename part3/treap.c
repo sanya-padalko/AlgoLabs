5fb925b33588c20063f5ee2786efa73ee66bc0a0
bestronaldo <apadalko90@gmail.com>
2026-05-09 19:15:01 +0300

    [ Part 3 ] Реализация декартова дерева

---
+++ part3/treap.c
@@ -0,0 +1,119 @@
+#include "treap.h"
+
+DDnode* Insert(DDnode* node, int val) {
+	if (Check(&node, val))	return node;
+	
+	DDpair res = Split(node, val);
+
+	return Merge(Merge(res.first, DDnodeCtor(val)), res.second);
+}
+
+DDnode* Delete(DDnode* node, int val) {
+	DDpair res	= Split(node, val);
+	DDpair res2 = Split(res.second, val + 1);
+
+	return Merge(res.first, res2.second);
+}
+
+int Check(DDnode** node, int val) {
+	DDpair res	= Split(*node, val);
+	DDpair res2	= Split(res.second, val + 1);
+
+	int result	= (int)(res2.first != NULL);
+	*node = Merge(res.first, Merge(res2.first, res2.second));
+
+	return result;
+}
+
+DDpair Split(DDnode* node, int val) {
+	if (!node)	return DDpairCtor(NULL, NULL);
+
+	if (GetVal(node) >= val) {
+		DDpair res = Split(GetLeft(node), val);
+		node->left = res.second;
+
+		return DDpairCtor(res.first, node);
+	}
+	else {
+		DDpair res = Split(GetRight(node), val);
+		node->right = res.first;
+		
+		return DDpairCtor(node, res.second);
+	}
+}
+
+DDnode* Merge(DDnode* node1, DDnode* node2) {
+	if (!node1)	return node2;
+	if (!node2)	return node1;
+
+	if (GetPrior(node1) > GetPrior(node2)) {
+		node1->right = Merge(GetRight(node1), node2);
+
+		return node1;
+	}
+	else {
+		node2->left = Merge(node1, GetLeft(node2));
+
+		return node2;
+	}
+}
+
+void ClearTree(DDnode* node) {
+	if (!node)	return;
+
+	ClearTree(GetLeft(node));
+	ClearTree(GetRight(node));
+	
+	free(node);
+}
+
+DDnode* DDnodeCtor(int val) {
+	DDnode* node	=	(DDnode*)calloc(1, sizeof(DDnode));
+
+	node->val		=	val;
+	node->prior		=	rand();
+	
+	node->left		=	node->right	=	NULL;
+
+	return node;
+}
+
+void DDnodeDtor(DDnode* node) {
+	node->val = node->prior = 0;
+	node->left = node->right = NULL;
+
+	free(node);
+}
+
+DDpair DDpairCtor(DDnode* first, DDnode* second) {
+	DDpair* pair_ptr	=	(DDpair*)calloc(1, sizeof(DDpair));
+
+	pair_ptr->first		=	first;
+	pair_ptr->second	=	second;
+
+	return *pair_ptr;
+}
+
+DDnode* GetLeft(DDnode* node) {
+	if (!node)	return NULL;
+
+	return node->left;
+}
+
+DDnode* GetRight(DDnode* node) {
+	if (!node)	return NULL;
+
+	return node->right;
+}
+
+int GetVal(DDnode* node) {
+	if (!node)	return 0;
+
+	return node->val;
+}
+
+int GetPrior(DDnode* node) {
+	if (!node)	return 0;
+
+	return node->prior;
+}
---
+++ part3/treap.h
@@ -0,0 +1,36 @@
+#ifndef _TREAP_H_
+#define _TREAP_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct DDnode {
+	int val;
+	int prior;
+	struct DDnode* left;
+	struct DDnode* right;
+} DDnode;
+
+typedef struct DDpair {
+	DDnode* first;
+	DDnode* second;
+} DDpair;
+
+DDnode*		Insert		(DDnode* node, int val)			;
+DDnode*		Delete		(DDnode* node, int val)			;
+int			Check		(DDnode** node, int val)		;
+
+DDpair		Split		(DDnode* node, int val)			;
+DDnode*		Merge		(DDnode* node1, DDnode* node2)	;
+
+DDnode*		DDnodeCtor	(int val)						;
+void		DDnodeDtor	(DDnode* node)					;
+DDpair		DDpairCtor	(DDnode* first, DDnode* second)	;
+
+DDnode*		GetLeft		(DDnode* node)					;
+void		ClearTree	(DDnode* node)					;
+DDnode*		GetRight	(DDnode* node)					;
+int			GetVal		(DDnode* node)					;
+int			GetPrior	(DDnode* node)					;
+
+#endif
