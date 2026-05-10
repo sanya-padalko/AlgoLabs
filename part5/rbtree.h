5fcb76f799e7f7d4126decdc38ccc95e58c549e6
bestronaldo <apadalko90@gmail.com>
2026-05-10 21:23:10 +0300

    [ Part 5 ] Реализация красно-чёрного дерева

---
+++ part5/rbtree.h
@@ -0,0 +1,30 @@
+#ifndef _RBTREE_H_
+#define _RBTREE_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef enum { RED, BLACK } Color;
+
+typedef struct RBnode {
+	int		val;
+	Color	color;
+	struct RBnode* left;
+	struct RBnode* right;
+	struct RBnode* parent;
+} RBnode;
+
+RBnode*     Insert          (RBnode* root, int val);
+RBnode*     Delete          (RBnode* root, int val);
+void        ClearTree       (RBnode* root);
+
+RBnode*     RBnodeCtor      (int val);
+void        NodeDtor        (RBnode* node);
+
+RBnode*     GetLeft         (RBnode* node);
+RBnode*     GetRight        (RBnode* node);
+RBnode*     GetParent       (RBnode* node);
+int         GetVal          (RBnode* node);
+Color       GetColor        (RBnode* node);
+
+#endif
