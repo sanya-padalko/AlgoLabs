91c3490ec7c1bb0edd83326f976ea643aa005769
bestronaldo <apadalko90@gmail.com>
2026-05-11 18:47:13 +0300

    Bash-скрипты дописаны

--- table_test.sh
+++ table_test.sh
@@ -6,7 +6,7 @@ fi
 
 python3 table_test.py
 echo "Tests generated"
-gcc table_test.c -o table_test
+gcc table_test.c hash_func.c hash_tables/*.c -o table_test
 ./table_test
 python3 table_plot_1.py
 python3 table_plot_2.py
--- test_load.sh
+++ test_load.sh
@@ -4,5 +4,5 @@ if [[ ! -d "hash_table_res" ]]; then
     mkdir "hash_table_res"
 fi
 
-gcc hash_table_load.c -o table_load
+gcc hash_table_load.c hash_func.c hash_tables/*.c -o table_load
 ./table_load
