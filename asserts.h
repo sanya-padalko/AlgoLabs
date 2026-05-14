8bba1f597020cd2fff62c73fcaf4b050bfe0ecdb
bestronaldo <apadalko90@gmail.com>
2026-05-14 22:33:01 +0300

    [  ASSERT  ] Добавлены удобные ассерты

---
+++ asserts.h
@@ -0,0 +1,21 @@
+#ifndef _ASSERTS_H_
+#define _ASSERTS_H_
+
+#include <assert.h>
+#include <stdio.h>
+
+#ifdef DEBUG
+	#define ASSERT(condition, ...)									\
+		do {														\
+			if (!(condition)) {										\
+				fprintf(stderr, "ASSERTION FAILED in %s (%s:%d)\n",	\
+						__func__, __FILE__, __LINE);				\
+				fprintf(stderr, __VA_ARGS__);						\
+				assert(0);											\
+			}														\
+		} while (0)
+#else
+	#define ASSERT(condition, ...)
+#endif
+
+#endif
