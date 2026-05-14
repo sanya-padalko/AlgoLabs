1dd04f39a4346a66031283b975970498202c4d62
bestronaldo <apadalko90@gmail.com>
2026-05-14 23:53:24 +0300

    [  RSQ  ] Реализовано дерево Фенвика

---
+++ RSQ/fenwick.c
@@ -0,0 +1,60 @@
+#include "fenwick.h"
+
+Fenwick* FenArrCtor(int size, int* arr) {
+	Fenwick* tree = FenCtor(size);
+	TREE_VERIFY(tree);
+
+	for (int ind = 0; ind < size; ++ind)
+		AddVal(tree, ind, arr[ind]);
+	
+	TREE_VERIFY(tree);
+	return tree;
+}
+
+void AddVal(Fenwick* tree, int ind, int delta) {
+	TREE_VERIFY(tree);
+
+	for (; ind <= tree->size; ind += ind & -ind)
+		tree->pref[ind].sum += delta;
+
+	TREE_VERIFY(tree);
+}
+
+int SegSum(Fenwick* tree, int lt, int rt) {
+	TREE_VERIFY(tree);
+	MY_ASSERT(lt >= 0 && rt <= tree->size, "Недопустимые границы отрезка\n");
+
+	return PrefSum(tree, rt) - PrefSum(tree, lt - 1);
+}
+
+int PrefSum(Fenwick* tree, int ind) {
+	TREE_VERIFY(tree);
+	MY_ASSERT(ind <= tree->size, "Недопустимый префикс\n");
+
+	int sum = NEUTRAL_ELEM;
+	for (; ind > 0; ind -= ind & -ind)
+		sum += tree->pref[ind].sum;
+	
+	TREE_VERIFY(tree);
+	return sum;
+}
+
+Fenwick* FenCtor(int size) {
+	MY_ASSERT(size >= 0, "Недопустимый размер дерева");
+
+	Fenwick* tree = (Fenwick*)calloc(1, sizeof(Fenwick));
+
+	tree->size = size;
+	tree->pref = (FenNode*)calloc(size + 1, sizeof(FenNode));
+
+	return tree;
+}
+
+void FenDtor(Fenwick* tree) {
+	TREE_VERIFY(tree);
+
+	tree->size = 0;
+	free(tree->pref);
+
+	free(tree);
+}
---
+++ RSQ/fenwick.h
@@ -0,0 +1,37 @@
+#ifndef _FENWICK_H_
+#define _FENWICK_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "../asserts.h"
+
+#define NEUTRAL_ELEM	0
+
+typedef struct FenNode {
+	int sum;
+} FenNode;
+
+typedef struct Fenwick {
+	int	size;
+	FenNode* pref;
+} Fenwick;
+
+#ifdef DEBUG
+	#define TREE_VERIFY(tree)										\
+		MY_ASSERT(tree,			"Нулевой указатель на дерево\n");	\
+		MY_ASSERT(tree->size,	"Пустое дерево\n");					\
+		MY_ASSERT(tree->pref, 	"Дерево без элементов\n");				
+#else
+	#define FEN_VERIFY(fen)
+#endif
+
+Fenwick*	FenArrCtor	(int size, int* arr)				;
+
+void		AddVal		(Fenwick* tree, int ind, int delta)	;
+int			SegSum		(Fenwick* tree, int lt, int rt)		;
+int			PrefSum		(Fenwick* tree, int ind)			;
+
+Fenwick*	FenCtor		(int size)							;
+void		FenDtor		(Fenwick* tree)						;
+
+#endif
